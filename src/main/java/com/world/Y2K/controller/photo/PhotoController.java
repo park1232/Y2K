984f65d4b2617de6ade67a4366bb8ab431b046ad
Ssantoo <nowing0108@kakao.com>
2022-11-19 18:35:16 +0900

    feat photoController and photoService and photoServiceImpl

--- src/main/java/com/world/Y2K/controller/photo/PhotoController.java
+++ src/main/java/com/world/Y2K/controller/photo/PhotoController.java
@@ -1,29 +1,63 @@
 package com.world.Y2K.controller.photo;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.world.Y2K.model.vo.Photo;
+import com.world.Y2K.service.photo.PhotoService;
+
+
+
 
 @Controller
 public class PhotoController {
 	
-	@GetMapping("/modal.ph")
-	public String test() {
-		return "modal";
-	}
+	@Autowired
+	private PhotoService pService;
 	
 	@GetMapping("/photo.ph")
-	public String test2() {
+	public String photo() {
+		
+	
 		return "photo";
 	}
 	
 	@GetMapping("/show.ph")
 	public String test3() {
+		
+		
 		return "show";
 	}
 	
 	@GetMapping("/upload.ph")
-	public String test4() {
+	public String upload() {
+
 		return "upload";
 	}
+	
+	@PostMapping("/imageUpload.ph")
+	public String imageUpload(@ModelAttribute Photo p,
+			@RequestParam("file") MultipartFile files,
+			HttpServletRequest request,
+			Model model) {
+		
+		if(files.isEmpty()) {
+			throw new PhotoException("이미지가 첨부되지않았습니다.");
+		}
+		
+		pService.insertImage(p, files, request);
+		
+	
+		
+		
+		return null;
+	}
 }
-
---
+++ src/main/java/com/world/Y2K/service/photo/PhotoService.java
@@ -0,0 +1,12 @@
+package com.world.Y2K.service.photo;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.world.Y2K.model.vo.Photo;
+
+public interface PhotoService {
+
+	void insertImage(Photo p, MultipartFile files, HttpServletRequest request);
+}
