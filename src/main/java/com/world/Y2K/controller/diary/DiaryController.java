6ecc317cc81ee1807b3c1a7afe58f03db921d1f1
min19960 <minju4687@gmail.com>
2022-11-21 20:23:50 +0900

    chore: diary3.css diary.jsp diary2.jsp diary3.jsp feat: DiaryController DiaryService

--- src/main/java/com/world/Y2K/controller/diary/DiaryController.java
+++ src/main/java/com/world/Y2K/controller/diary/DiaryController.java
@@ -1,26 +1,34 @@
 package com.world.Y2K.controller.diary;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.world.Y2K.service.diary.DiaryService;
 
 @Controller
 public class DiaryController {
-
+	
+	@Autowired
+	private DiaryService dService;
+	
 	@GetMapping("/diary.di")
 	public String test1() {
-		System.out.println("test");
-		System.out.println("test2");
 		return "diary";
+	}
+	
+	@RequestMapping("/diary3.di")
+	public String diaryWrite(@RequestParam("datepicker") String datepicker, Model model) {
+		model.addAttribute("datepicker", datepicker);
+		return "diary3";
 	}
 	
 	@GetMapping("/diary2.di")
 	public String test2() {
 		return "diary2";
-	}
-	
-	@RequestMapping("/diary3.di")
-	public String test3() {
-		return "diary3";
 	}
 }
