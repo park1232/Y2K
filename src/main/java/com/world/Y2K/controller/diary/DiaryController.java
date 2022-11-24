5277779607b778a3eaa01c9258e1c442ac88abc1
min19960 <minju4687@gmail.com>
2022-11-24 21:46:15 +0900

    feat: RootConfig.java DiaryController diary-mapper.xml chore: diary3.css diary.jsp Diary.java

--- src/main/java/com/world/Y2K/controller/diary/DiaryController.java
+++ src/main/java/com/world/Y2K/controller/diary/DiaryController.java
@@ -1,15 +1,24 @@
 package com.world.Y2K.controller.diary;
 
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
+import com.world.Y2K.model.vo.Diary;
 import com.world.Y2K.service.diary.DiaryService;
+import com.world.Y2K.service.login.auth.UserDetailsImpl;
 
+@SessionAttributes("loginUser")
 @Controller
 public class DiaryController {
 	
@@ -25,6 +34,7 @@ public class DiaryController {
 	public String diaryWrite(@RequestParam("datepicker") String datepicker, @RequestParam("mapValue") String mapValue, Model model) {
 		model.addAttribute("datepicker", datepicker);
 		model.addAttribute("mapValue", mapValue);
+		
 		return "diary/diary3";
 	}
 	
@@ -34,7 +44,16 @@ public class DiaryController {
 	}
 	
 	@RequestMapping("/insertDiary.di")
-	public String insertDiary() {
+	public String insertDiary(@ModelAttribute Diary d, Authentication authentication, HttpServletRequest request, Model model) {
+		UserDetailsImpl userDetails = (UserDetailsImpl)authentication.getPrincipal();
+		System.out.println("user:" + userDetails.getMember().getUsername());
+//		String diaryWriter = ((Member)userDetails.getMember()).getUsername();
+//		ModelAndView mv = new ModelAndView();
+//		mv.setViewName("diary/diary");
+//		mv.addObject("loginUser",userDetails.getMember());
+		HttpSession session =  request.getSession();
+		session.setAttribute("loginUser", userDetails.getMember());
+		model.getAttribute("loginUser");
 		return "diary/diary";
 	}
 	
