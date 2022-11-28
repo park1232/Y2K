4047f257947d0c276e6910813b1ae9494f4981fb
Ssantoo <nowing0108@kakao.com>
2022-11-28 18:57:10 +0900

    feat mainPage reply

---
+++ src/main/java/com/world/Y2K/model/dto/MainRe.java
@@ -0,0 +1,18 @@
+package com.world.Y2K.model.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class MainRe {
+	
+	private String content;
+	
+	private String nickName;
+	
+	private Long replyWriter;
+	
+}
--- src/main/java/com/world/Y2K/service/main/MainServiceImpl.java
+++ src/main/java/com/world/Y2K/service/main/MainServiceImpl.java
@@ -1,13 +1,14 @@
 package com.world.Y2K.service.main;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.world.Y2K.dao.main.MainDAO;
-
 import com.world.Y2K.model.vo.Reply;
 
 
@@ -26,19 +27,29 @@ public class MainServiceImpl implements MainService{
 	private MainDAO mDAO;
 
 	@Override
-	public ArrayList<Reply> replyList(Long boardNo) {
+	public ArrayList<Reply> replyList(Long userNo) {
 		
-		System.out.println(boardNo);
+		System.out.println(userNo);
 		
-		return mDAO.replyList(sqlSession, boardNo);
+		return mDAO.replyList(sqlSession, userNo);
 	}
 
 	@Override
-	public void insertReply(Reply r) {
+	public Map<String,Object> insertReply(String content, String nickName, Long replyWriter) {
 		
-		System.out.println(r);
+		HashMap<String, Object> map = new HashMap<String,Object>();
 		
+		map.put("content", content);
+		map.put("nickname", nickName);
+		map.put("userNo", replyWriter);
 		
+		int result = mDAO.insertReply(sqlSession, map);
+//		System.out.println(result);
+//		
+//		System.out.println("mapÀ§Ä¡"+ map);
+//		System.out.println(map);
+		
+		return map;
 	}
 	
 }
